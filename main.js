1b8980fbd2f751a92a35ad3eff201c9b8783ffd1
AliServat <aliservat20@gmail.com>
2021-10-20 11:49:24 +0330

    got the App working...it could use some improvements though.

--- main.js
+++ main.js
@@ -4,9 +4,35 @@ const date = document.querySelector('#date');
 const amount = document.querySelector('#amount');
 const table = document.querySelector('table');
 
+let counter = 0;
+let tr;
+
 button.addEventListener('click',(e)=>{
     e.preventDefault();
-    const td = document.createElement('td');
-    td.appendChild(document.createTextNode(name.value));
-    table.appendChild(td);
+    
+    tr = document.createElement('tr');
+    const td1 = InsertInputValue(name);
+    const td2 = InsertInputValue(date);
+    const td3 = InsertInputValue(amount);
+    const td4 = document.createElement('td');
+    const deletebtn = document.createElement('button');
+    deletebtn.innerHTML = 'Remove';
+    deletebtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+
+        deletebtn.parentNode.parentNode.parentNode.removeChild(deletebtn.parentNode.parentNode);
+    })
+    td4.appendChild(deletebtn);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    table.appendChild(tr);
+    
 })
+
+const InsertInputValue = function(input){
+const td = document.createElement('td');
+    td.appendChild(document.createTextNode(input.value));
+    return td;
+}
